b5e555f42388de2a72afc5543df2a31baa8f3d48
Ahmadjon Qudratov <qahmadjon11@gmail.com>
2026-04-28 14:17:56 +0500

    ConnectionStrings fix

--- src/NavQurt.Infrastructure/ConnectionStrings.cs
+++ src/NavQurt.Infrastructure/ConnectionStrings.cs
@@ -2,5 +2,5 @@ namespace NavQurt.Infrastructure;
 
 internal static class ConnectionStrings
 {
-    public const string LocalMain = "Server=127.0.0.1;port=5432;Database=main_db;Uid=postgres;Pwd=root";
+    public const string LocalMain = "Server=127.0.0.1;port=5432;Database=navqurt_db;Uid=postgres;Pwd=1";
 }
